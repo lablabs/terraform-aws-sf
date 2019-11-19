659d3a9e935e5c0f6b57bd71d0199c4f834be8a9
Martin <hauskrechtmartin@gmail.com>
2019-11-19 17:12:53 +0100

    Add ENI ids to outputs

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,14 @@ output "eni-az2-ip" {
   value = length(var.aws_zones) > 2 ? sort(aws_network_interface.az2[0].private_ips)[0] : ""
 }
 
+output "eni-az0-id" {
+  value = aws_network_interface.az0[0].id
+}
+
+output "eni-az1-id" {
+  value = length(var.aws_zones) > 1 ? aws_network_interface.az1[0].id : ""
+}
+
+output "eni-az2-id" {
+  value = length(var.aws_zones) > 2 ? aws_network_interface.az2[0].id : ""
+}

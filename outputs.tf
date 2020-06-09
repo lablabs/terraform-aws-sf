af868ed981a8fcc993c47030a6e86cdfb638f410
Karol Bujaček <karol@bujacek.sk>
2020-06-09 15:11:44 +0200

    Generate Inputs and Outputs in the README file automatically
    Variables and outputs files are updated (added descriptions, fixed typos, etc.)

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,29 @@
 output "eni-az0-ip" {
-  value = sort(aws_network_interface.az0[0].private_ips)[0]
+  description = "IP address of the ENI in AZ0"
+  value       = sort(aws_network_interface.az0[0].private_ips)[0]
 }
 
 output "eni-az1-ip" {
-  value = length(var.aws_zones) > 1 ? sort(aws_network_interface.az1[0].private_ips)[0] : ""
+  description = "IP address of the ENI in AZ1"
+  value       = length(var.aws_zones) > 1 ? sort(aws_network_interface.az1[0].private_ips)[0] : ""
 }
 
 output "eni-az2-ip" {
-  value = length(var.aws_zones) > 2 ? sort(aws_network_interface.az2[0].private_ips)[0] : ""
+  description = "IP address of the ENI in AZ2"
+  value       = length(var.aws_zones) > 2 ? sort(aws_network_interface.az2[0].private_ips)[0] : ""
 }
 
 output "eni-az0-id" {
-  value = aws_network_interface.az0[0].id
+  description = "ID of the ENI in AZ0"
+  value       = aws_network_interface.az0[0].id
 }
 
 output "eni-az1-id" {
-  value = length(var.aws_zones) > 1 ? aws_network_interface.az1[0].id : ""
+  description = "ID of the ENI in AZ1"
+  value       = length(var.aws_zones) > 1 ? aws_network_interface.az1[0].id : ""
 }
 
 output "eni-az2-id" {
-  value = length(var.aws_zones) > 2 ? aws_network_interface.az2[0].id : ""
+  description = "ID of the ENI in AZ2"
+  value       = length(var.aws_zones) > 2 ? aws_network_interface.az2[0].id : ""
 }

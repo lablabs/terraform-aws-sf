83492a3c3b1ff55bb0e5b690d35a948b44f7ed75
Karol Bujaček <karol@bujacek.sk>
2020-06-10 16:16:18 +0200

    Added basic example and updated readme file

---
+++ examples/basic/main.tf
@@ -0,0 +1,126 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_iam_instance_profile" "kafka" {
+  name = "kafka_instance_profile"
+  role = aws_iam_role.kafka.name
+}
+
+resource "aws_iam_role" "kafka" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "kafka" {
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:DescribeInstances"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "kafka" {
+  name       = "kafka-policy-attachment"
+  policy_arn = aws_iam_policy.kafka.arn
+  roles      = [aws_iam_role.kafka.name]
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh-key"
+  public_key = var.ssh_public_key
+}
+
+data "aws_s3_bucket" "lambda" {
+  bucket = var.s3_bucket
+}
+
+resource "aws_security_group" "kafka" {
+  vpc_id = aws_vpc.kafka.id
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = -1
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc" "kafka" {
+  cidr_block = var.vpc_base_cidr
+}
+
+resource "aws_subnet" "kafka" {
+  count = length(var.aws_zones)
+
+  cidr_block              = cidrsubnet(var.vpc_base_cidr, 4, count.index)
+  availability_zone       = var.aws_zones[count.index]
+  vpc_id                  = aws_vpc.kafka.id
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.kafka.id
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.kafka.id
+}
+
+resource "aws_route" "this" {
+  route_table_id         = aws_route_table.this.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_main_route_table_association" "this" {
+  route_table_id = aws_route_table.this.id
+  vpc_id         = aws_vpc.kafka.id
+}
+
+module "sf" {
+  source                  = "../.."
+  aws_region              = var.aws_region
+  ami                     = var.ami
+  aws_zones               = var.aws_zones
+  iam_instance_profile_id = aws_iam_instance_profile.kafka.id
+  key_pair_id             = aws_key_pair.ssh_key.id
+  lambda_s3_bucket        = data.aws_s3_bucket.lambda.id
+  lambda_s3_bucket_key    = var.s3_bucket_key
+  name                    = var.stack_name
+  node_count              = var.node_count
+  sg_ids                  = [aws_security_group.kafka.id]
+  stack_name              = var.name
+  subnets                 = { for s in aws_subnet.kafka : s.availability_zone => s.id }
+  vpc_id                  = aws_vpc.kafka.id
+  instance_type           = var.instance_type
+}
---
+++ examples/basic/outputs.tf
@@ -0,0 +1,29 @@
+output "eni-az0-ip" {
+  description = "IP address of the ENI in AZ0"
+  value       = module.sf.eni-az0-ip
+}
+
+output "eni-az1-ip" {
+  description = "IP address of the ENI in AZ1"
+  value       = module.sf.eni-az1-ip
+}
+
+output "eni-az2-ip" {
+  description = "IP address of the ENI in AZ2"
+  value       = module.sf.eni-az2-ip
+}
+
+output "eni-az0-id" {
+  description = "ID of the ENI in AZ0"
+  value       = module.sf.eni-az0-ip
+}
+
+output "eni-az1-id" {
+  description = "ID of the ENI in AZ1"
+  value       = module.sf.eni-az1-ip
+}
+
+output "eni-az2-id" {
+  description = "ID of the ENI in AZ2"
+  value       = module.sf.eni-az2-ip
+}
